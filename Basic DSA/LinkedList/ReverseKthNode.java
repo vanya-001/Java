b44494fd4ec14c25c4876b66051b087eb705bda3
Vanya Maheshwari <vanimaheshwari19@gmail.com>
2023-08-25 13:28:49 +0530

    Comments added

--- Basic DSA/LinkedList/ReverseKthNode.java
+++ Basic DSA/LinkedList/ReverseKthNode.java
@@ -3,3 +3,10 @@ package LinkedList;
 public class ReverseKthNode {
 
 }
+
+// Reverse linked list in alternate groups of k.
+// I created a function of reversing the linked list. I then traversed the
+// linked list and created a loop till k and reversed the list and also kept a
+// bool value for checking whether it was the first k nodes and for that saved
+// the head and then for next k nodes made no changes and so on till NULL and
+// returned head.
